7ac05d0ada918567176511adf41c96c22eb77047
Richard Palacios G <apalaciosg91@gmail.com>
2021-11-03 16:05:02 -0500

    feat: add cors * to manage cross-origin resources

--- nodepop/app.js
+++ nodepop/app.js
@@ -1,13 +1,12 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const { error404Handler,generalErrorHandler } = require("./middleware");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const { error404Handler, generalErrorHandler } = require("./middleware");
 const routes = require("./routes");
 const config = require("config");
-
-var app = express();
+const cors = require("cors");
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -20,21 +19,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors);
 
 /**
  * Conexión a la base de datos
  */
 require("./services/mongoDB");
-// require("./apiServices/anuncios/model");
-// require("./apiServices/anuncios/controller");
 
 //TODO: add documentation with swagger https://www.youtube.com/watch?v=S8kmHtQeflo&t=859s
 
 /**
  * Rutas de mi API
  */
-// app.use("/api/anuncios", require("./routes/api/routes"));
-// app.use("/api/", routes);
 app.use(config.get("App.restApiEndpoint.version1AnunciosPath"), routes);
 
 // Variables globales para vistas
@@ -43,7 +39,7 @@ app.locals.title = "NodePop";
 /**
  * Rutas de mi aplicación web
  */
-app.use("/",require("./routes/index"));
+app.use("/", require("./routes/index"));
 app.use(error404Handler); // catch 404 and forward to error handler
 app.use(generalErrorHandler); // error handler
 
