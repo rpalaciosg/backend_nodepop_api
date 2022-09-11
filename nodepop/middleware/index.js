ce5cf6daef14d4da515115b6677da31fd008698a
Richard Palacios G <apalaciosg91@gmail.com>
2022-09-11 04:59:34 -0500

    hotfix(error): Fix import of http-errors

--- nodepop/middleware/index.js
+++ nodepop/middleware/index.js
@@ -1,21 +1,22 @@
 "use strict";
+var createError = require("http-errors");
 
 module.exports.error404Handler = function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 };
 
-module.exports.generalErrorHandler = function(err, req, res, next) {
+module.exports.generalErrorHandler = function (err, req, res, next) {
   // comprueba error de validación
   if (err.array) {
     err.status = 422;
     const errInfo = err.array({ onlyFirstError: true })[0];
     err.message = isAPI(req)
-      ? {message: "Not valid", errors: err.mapped()}
+      ? { message: "Not valid", errors: err.mapped() }
       : `Not valid - ${errInfo.param} ${errInfo.msg}`;
   }
   res.status(err.status || 500);
-  if(isAPI(req)) {
-    res.json({ success: false, error: err.message});
+  if (isAPI(req)) {
+    res.json({ success: false, error: err.message });
     return;
   }
   // set locals, only providing error in development
@@ -23,7 +24,7 @@ module.exports.generalErrorHandler = function(err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.render("error");
-}; 
+};
 
 // función para saber si es una petición a un API
 function isAPI(req) {
